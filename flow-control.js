eee2b4d25bca531cb6afbff9556a969bfef983cf
Rowan Taylor <rowan.taylor@colorado.edu>
2019-02-18 23:05:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,9 +25,17 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else {
+    return "You are not a teenager"
+  }
 }
 
 function switchAge(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else {
+    return "You have an age"
+  }
 }
